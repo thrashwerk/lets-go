6d42495b02613a02214649cf89d02b0cc3919b92
Deividas Burškaitis <thrashwerk@mail25.net>
2024-12-29 02:38:08 +0200

    replace standard logging with a structured logger

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelInfo,
+	}))
 
 	mux := http.NewServeMux()
 
@@ -21,8 +27,9 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("starting server on %s", *addr)
+	logger.Info("starting server", slog.String("addr", *addr))
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
