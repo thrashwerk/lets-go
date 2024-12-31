4dc1e0634258e796c3398358e10bcbc3ebbb7bb1
Deividas Burškaitis <thrashwerk@mail25.net>
2024-12-31 13:08:24 +0200

    add conditions to display latest snippets if there are any

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/thrashwerk/lets-go/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
