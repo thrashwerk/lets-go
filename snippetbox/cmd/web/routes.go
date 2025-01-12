9d108bfb435b7d982b6ce458f2b2b03512ee4558
Deividas Burškaitis <thrashwerk@mail25.net>
2025-01-12 22:51:57 +0200

    use context to check if a user authenticated and if they have a real user id

--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -13,7 +13,7 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	// no auth
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
