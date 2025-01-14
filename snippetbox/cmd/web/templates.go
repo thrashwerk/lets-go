1f2719205bd517bbc19a13a5ae105027f85f63e5
Deividas Burškaitis <thrashwerk@mail25.net>
2025-01-14 11:08:26 +0200

    add a unit test for humanDate function

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -21,8 +21,12 @@ type templateData struct {
 }
 
 func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+
 	// return t.Format("02 Jan 2006 at 15:04")
-	return t.Format("2006-01-02 15:04")
+	return t.UTC().Format("2006-01-02 15:04")
 }
 
 var functions = template.FuncMap{
